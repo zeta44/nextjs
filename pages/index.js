fedbb1113577061ee501f0132bec83af732d952b
José Nigério Evangelista Lima Bezerra <nigerio@nimolabs.net>
2022-05-23 08:10:18 +0200

    mobile

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header>
-        <section className={styles.header}>
+      <header className={styles.header}>
+        <section>
           <p className={styles.title_top}>
             Luctus vitae.
           </p>
@@ -50,6 +50,7 @@ export default function Home() {
               Suspendisse tincidunt vulputate ex, a laoreet neque luctus vitae. Quisque maximus ante sapien, at pharetra sem sagittis eu. Maecenas nibh leo, interdum ac malesuada quis, tempus vel lacus.
 
             </p>
+            
             <p>
               Aliquam quis velit et nunc scelerisque auctor quis id lacus. Proin sem turpis, rutrum ut placerat nec, varius sit amet lacus. Praesent sed viverra lorem.
 
@@ -108,11 +109,20 @@ export default function Home() {
           </article>
 
           <article className={styles.servicesImages}>
+            
             <div className={styles.square_1}>
 
             </div>
 
             <div className={styles.square_2}>
+
+            </div>
+
+            <p>
+              Aliquam vel orci pulvinar, porta velit in, volutpat neque. Nam vel leo nec nibh tempus congue. Morbi rhoncus bibendum enim sit amet pulvinar. Quisque lobortis commodo nunc, a rhoncus diam aliquam ac. Pellentesque non augue ullamcorper arcu facilisis bibendum. Nulla quis tincidunt turpis. Sed quis erat arcu. Phasellus gravida felis quis ex mattis, sed suscipit tellus tincidunt. In imperdiet dapibus vehicula. In auctor nec nibh vestibulum ornare. Morbi porta, enim hendrerit consectetur ultrices, libero dui ultricies libero, vel rutrum sem magna quis augue. Nulla quis magna at lorem sodales gravida.
+            </p>
+
+            <div className={styles.square_3}>
 
             </div>
           </article>
@@ -125,17 +135,19 @@ export default function Home() {
       </main>
 
 
-      <footer>
-        <section >
+      <footer className={styles.footer}>
+        <section>
           <div className={styles.main_footer_separator}>
             
           </div>
         </section>
-        <section className={styles.footer}>
+        <section className={styles.footer_bottom}>
+
           <article>
             <div className={styles.squareFooter}>
 
             </div>
+
             <div className={styles.squareFooter}>
 
             </div>
