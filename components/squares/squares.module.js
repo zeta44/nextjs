b7e32bf0f8e37c988e730eb8d5bcbc0d9368bc5c
José Nigério Evangelista Lima Bezerra <nigerio@nimolabs.net>
2022-05-25 15:47:50 +0200

    AOS Property

--- components/squares/squares.module.js
+++ components/squares/squares.module.js
@@ -10,11 +10,11 @@ export function Squares() {
         ></div>
         <div className={styles.b}
         data-aos="flip-left"
-        data-aos-duration="1000"
+        data-aos-duration="1500"
         ></div>
         <div className={styles.c}
         data-aos="flip-down"
-        data-aos-duration="1000"
+        data-aos-duration="1700"
         ></div>
         <div className={styles.d}
         data-aos="flip-right"
@@ -22,7 +22,7 @@ export function Squares() {
         ></div>
         <div className={styles.e}
         data-aos="flip-right"
-        data-aos-duration="1000"
+        data-aos-duration="2000"
         ></div>
       </article>
     </section>
